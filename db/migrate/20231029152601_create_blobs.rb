7152a24ffee1e8d58cb4e17cfbd1a34d53ff2220
Faisal Alashir <faisal.alashir@oivan.com>
2023-10-29 18:34:29 +0300

    fixing the migration

--- db/migrate/20231029152601_create_blobs.rb
+++ db/migrate/20231029152601_create_blobs.rb
@@ -1,10 +1,8 @@
 class CreateBlobs < ActiveRecord::Migration[6.1]
   def change
     create_table :blobs do |t|
-      t.string :id
       t.text :data
       t.integer :size
-      t.datetime :created_at
 
       t.timestamps
     end
