597f357e16145e8ff5f49bb33cb2a9c3f91d6180
Faisal Alashir <faisal.alashir@oivan.com>
2023-10-31 15:02:49 +0300

    remove unused code

--- app/models/blob.rb
+++ app/models/blob.rb
@@ -2,7 +2,6 @@ class Blob < ApplicationRecord
   has_one :storage_backend
 
   validates :blob_id, presence: true, uniqueness: { case_sensitive: false }
-  before_save :is_storage_backend_valid?
 
   def store_file()
     self.storage_backend.store_file(self)
